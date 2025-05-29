c222aa942ad6dfd02155aa1388b3529b2934cf19
MuhFikriansyah <162697186+MuhFikriansyah@users.noreply.github.com>
2025-05-29 17:51:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSection(id) {
+  const sections = document.querySelectorAll('.content-section');
+  sections.forEach(section => {
+    section.classList.add('hidden');
+  });
+
+  document.getElementById(id).classList.remove('hidden');
+}
